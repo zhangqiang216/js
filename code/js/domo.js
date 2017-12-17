db7519c8a394fe473d269f5711402f1cf8f9760f
张强 <3489927956@qq.com>
2017-12-17 18:47:59 +0800

    srcoll
    图片滚动

---
+++ code/js/domo.js
@@ -0,0 +1,37 @@
+﻿<!DOCTYPE HTML>
+<html>
+ <head>
+  <title>图片自动滚动</title>
+  <link rel="stylesheet" type="text/css" href="domo.css">
+
+  <style type="text/css">
+  	div
+  	{
+  		white-space: nowrap; 
+  		overflow:hidden;
+  		width: 1500px;
+  	}
+</style>
+
+ </head>
+
+ <body>
+ <div id="picScroll">
+	<span id="picScroll1">
+		<img src="images/1.jpg" />
+		<img src="images/2.jpg" />
+		<img src="images/3.jpg" />
+		<img src="images/4.jpg" />
+		<img src="images/5.jpg" />
+		<img src="images/6.jpg" />
+		<img src="images/7.jpg" />
+		<img src="images/8.jpg" />
+	</span>
+	<span id="picScroll2">
+	</span>
+ </div> 
+ </body>
+ <script src="domo.js"></script>
+ 
+ </script>
+</html>
